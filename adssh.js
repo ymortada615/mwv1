01396227f27742db26782f3e879120a121c4c9ef
ymortada615 <128693275+ymortada615@users.noreply.github.com>
2023-03-30 19:47:31 +0200

    Update adssh.js

--- adssh.js
+++ adssh.js
@@ -1,17 +1,1 @@
-
-
-        for(var e in els) {
-            if (!els.hasOwnProperty(e)) continue;
-
-            var el = els[e];
-            if (typeof el !== 'object') continue;
-
-            if (el.type === 'radio') {
-                if (el.value === val) el.checked = true;
-            } else {
-                el.value = val;
-                if (el.type === 'range') rangeChanged({target: el});
-            }
-        }
-    }
-    updatePreview();
+044444
